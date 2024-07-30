49de06404ee38265e4c2fbf0e0fdfc2323d6dc64
styrkink <styrkink@gmail.com>
2024-07-30 11:28:46 +0300

    combine reports for github pages

--- cypress.config.1280x720.js
+++ cypress.config.1280x720.js
@@ -3,19 +3,15 @@ const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
 const addCucumberPreprocessorPlugin = require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin;
 const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin;
 
-
-
 module.exports = defineConfig({
-
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
-    reportDir: 'cypress/reports',
+    reportDir: 'cypress/reports/custom',
     reportFilename: 'index',
     quiet: true,
     overwrite: false,
     html: true
   },
-  
   e2e: {
     async setupNodeEvents(on, config) {
       const bundler = createBundler({
